e22cc814e09e553f3f2b880a14ee886dd511ce2d
aneeshdalvi <adalvi1@asu.edu>
2019-07-29 03:17:01 -0700

    Added persistence to Favorites through redux

--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -3,13 +3,20 @@ import { Text, View, ScrollView, FlatList } from 'react-native';
 import { Card, Icon } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import { postFavorite } from '../redux/ActionCreators';
 
-const mapStateToProps = state => {
+  const mapStateToProps = state => {
     return {
       dishes: state.dishes,
-      comments: state.comments
+      comments: state.comments,
+      favorites: state.favorites
     }
   }
+
+  const mapDispatchToProps = dispatch => ({
+    postFavorite: (dishId) => dispatch(postFavorite(dishId))
+})
+
 
 function RenderDish(props) {
 
@@ -76,7 +83,7 @@ class Dishdetail extends Component {
     }
 
     markFavorite(dishId) {
-        this.setState({favorites: this.state.favorites.concat(dishId)});
+        this.props.postFavorite(dishId);
     }
 
     static navigationOptions = {
@@ -89,7 +96,7 @@ class Dishdetail extends Component {
             <ScrollView>
                 {/* some returns true if found in array */}
                 <RenderDish dish={this.props.dishes.dishes[+dishId]}
-                    favorite={this.state.favorites.some(el => el === dishId)}
+                    favorite={this.props.favorites.some(el => el === dishId)}
                     onPress={() => this.markFavorite(dishId)} 
                     />
                 <RenderComments comments={this.props.comments.comments.filter((comment) => comment.dishId === dishId)} />
@@ -98,4 +105,4 @@ class Dishdetail extends Component {
     }
 }
 
-export default connect(mapStateToProps)(Dishdetail);
+export default connect(mapStateToProps, mapDispatchToProps)(Dishdetail);
